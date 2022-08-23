80586b33a8907a3949ce99bf4ff0ae62f3811ab2
chenjiaxing <18974786549@163.com>
2022-08-23 17:25:53 +0800

    init pom

---
+++ src/main/java/com/dwlx/study/DwlxStudyApplication.java
@@ -0,0 +1,12 @@
+package com.dwlx.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwlxStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwlxStudyApplication.class, args);
+    }
+}
